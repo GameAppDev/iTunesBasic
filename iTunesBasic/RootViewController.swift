a2f889c07b98bb11141495d9dfd92607d946508b
Oguzhan <oguzz.ylcn@gmail.com>
2022-02-18 20:31:35 +0300

    pod init

---
+++ iTunesBasic/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+// RootViewController.swift
+// iTunesBasic
+//
+// Created on 18.02.2022.
+// Oguzhan Yalcin
+//
+//
+//
+
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
