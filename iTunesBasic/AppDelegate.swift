8275d72451c69c82f0930512620514e1c9f7d1f5
Oguzhan <oguzz.ylcn@gmail.com>
2022-02-18 20:31:55 +0300

    pod init

---
+++ iTunesBasic/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+// AppDelegate.swift
+// iTunesBasic
+//
+// Created on 18.02.2022.
+// Oguzhan Yalcin
+//
+//
+//
+
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
